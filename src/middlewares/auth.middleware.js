ea9849c9eb59505d98c32075823d9b6c27598ffc
pankaj-kb <pankajb2601@gmail.com>
2024-03-05 01:49:06 +0530

    fixed cookie issue

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,19 +1,18 @@
 import User from "../models/user.model.js";
-import jwt from "jsonwebtoken";
-
+import jwt from "jsonwebtoken"
 export const verifyJWT = async (req, res, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-
         if (!token) {
             const unauthorizedError = new Error("Unauthorized request");
             unauthorizedError.statusCode = 401;
             throw unauthorizedError;
         }
 
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const decodedToken = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
         const user = await User.checkExist(decodedToken?.username, decodedToken?.email);
+        console.log('User from auth:', user);
 
         if (!user.length) {
             const invalidTokenError = new Error("Invalid Access Token");
@@ -21,11 +20,11 @@ export const verifyJWT = async (req, res, next) => {
             throw invalidTokenError;
         }
 
-        req.user = user;
-
+        req.user = user[0];
         next();
     } catch (error) {
         const statusCode = error.statusCode || 401;
+        console.error('Error in verifyJWT:', error);
         throw new Error(statusCode, error?.message || "Invalid Access token");
     }
 };
